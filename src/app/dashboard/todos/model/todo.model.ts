aaf8823bb60a48f70a067452ba2d6000349a8f99
milos.brdar <milos.brdar@codecentric.de>
2017-08-18 11:11:32 +0200

    build created with ng build --base-href /angular-client-side-authentication --deploy /angular-client-side-authentication

--- src/app/dashboard/todos/model/todo.model.ts
+++
@@ -1,9 +0,0 @@
-export class Todo {
-  title: string;
-  content: string;
-
-  constructor(todo?: any) {
-    this.title = todo && todo.title || null;
-    this.content = todo && todo.content || null;
-  }
-}
--- src/app/shared/auth/model/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-  firstName: string;
-  lastName: string;
-
-  constructor(user?: any) {
-    this.firstName = user && user.firstName || null;
-    this.lastName = user && user.lastName || null;
-  }
-}
